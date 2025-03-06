3b93001aafc57e1f310ed20a05a8027c36492035
Shubham Prajapati <shubham528prajapati@gmail.com>
2025-03-06 23:16:32 +0530

    new commit

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
 export const NODE_API_ENDPOINT =
   import.meta.env.NODE_ENV === "production"
-    ? "https://shoping-app-backend.vercel.app/api"
+    ? "https://care360-backend.vercel.app/api"
     : "http://localhost:8900/api";
